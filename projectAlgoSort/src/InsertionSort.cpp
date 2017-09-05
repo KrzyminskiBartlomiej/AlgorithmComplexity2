37152a68ed20bbc6eaaf4c77f860c8335cb68317
Piotr Wroblewski <piotr.p.wroblewski@ericsson.com>
2017-09-05 11:34:08 +0200

    Added InsertionSort class

---
+++ projectAlgoSort/src/InsertionSort.cpp
@@ -0,0 +1,24 @@
+#include "InsertionSort.hpp"
+#include <time.h>
+#include <cstdlib>
+
+void InsertionSort::sort(std::vector<int> toSort) {
+	unsigned long long transitions = 0;
+	clock_t start, stop;
+	start = clock();
+	int tmp, tmpToCompare, sizeOfVector;
+	sizeOfVector = toSort.size();
+	for (int i = toSort.size() - 2; i >= 0; i--) {
+		tmp = toSort[i];
+		tmpToCompare = i + 1;
+		while ((tmp < sizeOfVector) && (tmp > toSort[tmpToCompare])) {
+			toSort[tmpToCompare - 1] = toSort[tmpToCompare];
+			tmpToCompare++;
+			transitions++;
+		}
+		toSort[tmpToCompare - 1] = tmp;
+	}
+	stop = clock();
+	mTime = (double) (stop - start) / CLOCKS_PER_SEC;
+	mTransitions = transitions;
+}
---
+++ projectAlgoSort/src/InsertionSort.hpp
@@ -0,0 +1,11 @@
+#ifndef INSERTIONSORT_HPP_
+#define INSERTIONSORT_HPP_
+
+#include "SortingAlgorithm.hpp"
+
+class InsertionSort: public SortingAlgorithm {
+public:
+	void sort(std::vector<int> toSort);
+};
+
+#endif /* INSERTIONSORT_HPP_ */
