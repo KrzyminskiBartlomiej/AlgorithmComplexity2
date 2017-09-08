51a68bc46d9808023cfb9d0dd8923dc296c48673
jan lepa <jan.lepa@ericsson.com>
2017-09-08 09:00:26 +0200

    Modified DataSetGenerator class-added new methods

--- projectAlgoSort/src/DataSetGenerator.cpp
+++ projectAlgoSort/src/DataSetGenerator.cpp
@@ -3,49 +3,83 @@
 DataSetGenerator::DataSetGenerator()
 {}
 
-void DataSetGenerator::createRandomSet(std::vector<int>& toFill, int n)
-{
-	for(int i=1; i<=n; ++i)
-	{
-		toFill.push_back(i);
-	}
-	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-	shuffle(toFill.begin(),toFill.end(),std::default_random_engine(seed));
-}
-
-void DataSetGenerator::createSortedSet(std::vector<int>& toFill, int n)
-{
-	for(int i=1; i<=n; ++i)
-	{
-		toFill.push_back(i);
+void DataSetGenerator::createSortedSet(std::vector<int>&toFill, unsigned long long vectorSize){
+	unsigned int valueToAdd = 0;
+    for(std::vector<int>::iterator it=toFill.begin(); it!=toFill.end(); ++it){
+		*it=valueToAdd++;
 	}
 }
 
-void DataSetGenerator::createBackwardSortedSet(std::vector<int>& toFill, int n)
-{
-	for(int i=n; i>0; --i)
-	{
-		toFill.push_back(i);
+void DataSetGenerator::extendSortedSet(std::vector<int>&toFill, unsigned long long toAdd){
+    int lastElementIndex = toFill.size();
+    int valueToAdd = toFill.back()+1;
+    toFill.resize(toFill.size()+toAdd);
+    for(unsigned int i=0; i<toAdd; ++i){
+       toFill[lastElementIndex++]=valueToAdd++;
+    }
+}
+
+void DataSetGenerator::createRandomSet(std::vector<int>& toFill, unsigned long long vectorSize){
+	for(std::vector<int>::iterator it=toFill.begin(); it!=toFill.end(); ++it){
+		*it=std::rand()%vectorSize;
 	}
 }
 
-void DataSetGenerator::createSortedSetWithFirstRandomValue(std::vector<int>& toFill, int n)
-{
-	toFill.push_back(std::rand()%n);
-	for(int i=1; i<n; ++i)
-	{
-		toFill.push_back(i);
+void DataSetGenerator::extendRandomSet(std::vector<int>& toFill, unsigned long long toAdd){
+    int lastElementIndex = toFill.size();
+    toFill.resize(toFill.size()+toAdd);
+	for(unsigned int i=1; i<toAdd; ++i){
+		toFill[lastElementIndex++]= std::rand()%toFill.size();
 	}
 }
 
-void DataSetGenerator::createBackwardSortedSetWithLastRandomValue(std::vector<int>& toFill, int n)
-{
-	for(int i=n; i>1; --i)
-	{
-			toFill.push_back(i);
+void DataSetGenerator::createBackwardSortedSet(std::vector<int>& toFill, unsigned long long vectorSize){
+	unsigned int valueToAdd = vectorSize-1;
+    for(std::vector<int>::iterator it=toFill.begin(); it!=toFill.end(); ++it){
+		*it=valueToAdd--;
 	}
-	toFill.push_back(std::rand()%n);
 }
+
+void DataSetGenerator::extendBackwardSortedSet(std::vector<int>& toFill, unsigned long long toAdd){
+	unsigned int valueToAdd=toFill.front();
+    for(unsigned int i=0; i<toAdd; ++i){
+    	toFill.insert(toFill.begin(),++valueToAdd);
+    }
+}
+
+void DataSetGenerator::createSortedSetWithFirstRandomValue(std::vector<int>& toFill, unsigned long long vectorSize){
+	toFill[0]=std::rand()%toFill.size();
+	unsigned long valueToAdd = 1;
+	for(std::vector<int>::iterator it=toFill.begin()+1; it!=toFill.end(); ++it){
+		*it=valueToAdd++;
+	}
+}
+
+void DataSetGenerator::extendSortedSetWithFirstRandomValue(std::vector<int>& toFill, unsigned long long toAdd){
+	extendSortedSet(toFill,toAdd);
+}
+
+void DataSetGenerator::createBackwardSortedSetWithLastRandomValue(std::vector<int>& toFill, unsigned long long vectorSize){
+	createBackwardSortedSet(toFill, vectorSize);
+	toFill.push_back(std::rand()%toFill.size());
+}
+
+void DataSetGenerator::extendBackwardSortedSetWithLastRandomValue(std::vector<int>& toFill, unsigned long long toAdd){
+	  toFill.pop_back();
+	  unsigned int valueToAdd=toFill.front();
+      for(unsigned int i=0; i<toAdd-1; ++i){
+	   	toFill.insert(toFill.begin(),++valueToAdd);
+	  }
+	  toFill.push_back(std::rand()%toFill.size());
+}
+
+
+
+
+
+
+
+
 
 
 
--- projectAlgoSort/src/DataSetGenerator.hpp
+++ projectAlgoSort/src/DataSetGenerator.hpp
@@ -10,11 +10,19 @@ class DataSetGenerator
 {
 public:
 	DataSetGenerator();
-	void createRandomSet(std::vector<int>& toFill, int n);
-	void createSortedSet(std::vector<int>& toFill, int n);
-	void createBackwardSortedSet(std::vector<int>& toFill, int n);
-	void createSortedSetWithFirstRandomValue(std::vector<int>& toFill, int n);
-	void createBackwardSortedSetWithLastRandomValue(std::vector<int>& toFill, int n);
+	void createRandomSet(std::vector<int>& toFill, unsigned long long vectorSize);
+	void createSortedSet(std::vector<int>&toFill, unsigned long long vectorSize);
+	void createBackwardSortedSet(std::vector<int>& toFill, unsigned long long vectorSize);
+	void createSortedSetWithFirstRandomValue(std::vector<int>& toFill, unsigned long long vectorSize);
+	void createBackwardSortedSetWithLastRandomValue(std::vector<int>& toFill, unsigned long long vectorSize);
+
+	void extendRandomSet(std::vector<int>& toFill, unsigned long long toAdd);
+	void extendSortedSet(std::vector<int>&toFill, unsigned long long toAdd);
+	void extendBackwardSortedSet(std::vector<int>& toFill, unsigned long long toAdd);
+	void extendSortedSetWithFirstRandomValue(std::vector<int>& toFill, unsigned long long toAdd);
+	void extendBackwardSortedSetWithLastRandomValue(std::vector<int>& toFill, unsigned long long toAdd);
 };
+
+
 
 #endif /* DATASETGENERATOR_HPP_ */
