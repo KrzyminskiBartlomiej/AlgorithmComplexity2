2a0e6b92a2410862f5042c767361c3cc816eb301
Piotr Wroblewski <piotr.p.wroblewski@ericsson.com>
2017-09-14 15:21:00 +0200

    Added working version of Bubble Sort

--- projectAlgoSort/src/BubbleSort.cpp
+++ projectAlgoSort/src/BubbleSort.cpp
@@ -1,29 +1,31 @@
+//============================================================================
+// Name        : BubbleSort.cpp
+// Description : C++ program for implementation of Bubble Sort
+//============================================================================
 #include "BubbleSort.hpp"
 
-void BubbleSort::sort(std::vector<int>toSort)
-{
-	unsigned long long transitions=0;
-	clock_t start,stop;
-	bool changed=true;
+// The main function with one argument to do Bubble Sort
+void BubbleSort::sort(std::vector<int> toSort) {
+	unsigned long long transitions = 0; // The flag counting transitions
+	clock_t start, stop; // Variables used to calculate the processor time consumed by the program
 
-	start=clock();
-	while(changed){
-		changed=false;
-		int tmp;
-		for(unsigned long long k=0; k<toSort.size(); ++k){
-			if(toSort[k]>toSort[k+1]){
-				tmp=toSort[k];
-				toSort[k]=toSort[k+1];
-				toSort[k+1]=tmp;
-				changed=true;
-				++transitions;
+	start = clock();
+	for (unsigned long long i = 0; i < toSort.size() - 1; i++) {
+
+		// One by one compare the adjacent elements
+		// Last i elements are already in place
+		for (unsigned long long j = 0; j < toSort.size() - i - 1; j++) {
+
+			// If an element is larger than next one
+			if (toSort[j] > toSort[j + 1]) {
+				std::swap(toSort[j], toSort[j + 1]);
+				transitions++;
 			}
 		}
 	}
-	stop=clock();
-	mTime = (double)(stop-start)/CLOCKS_PER_SEC;
+	stop = clock();
+
+	// Convert the processor time consumed by the program to the actual processing time of a program
+	mTime = (double) (stop - start) / CLOCKS_PER_SEC;
 	mTransitions = transitions;
 }
-
-
-
--- projectAlgoSort/src/BubbleSort.hpp
+++ projectAlgoSort/src/BubbleSort.hpp
@@ -2,10 +2,9 @@
 #define BUBBLESORT_HPP_
 #include"SortingAlgorithm.hpp"
 
-class BubbleSort: public SortingAlgorithm
-{
+class BubbleSort: public SortingAlgorithm {
 public:
-	void sort(std::vector<int>toSort);
+	void sort(std::vector<int> toSort);
 };
 
 #endif /* BUBBLESORT_HPP_ */
