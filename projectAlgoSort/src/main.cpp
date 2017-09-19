04acb9e8d7cfc40b41b4bb0a35ca830833386ade
Piotr Wroblewski <piotr.p.wroblewski@ericsson.com>
2017-09-19 09:26:06 +0200

    Added documentation for main.cpp file

--- projectAlgoSort/src/main.cpp
+++ projectAlgoSort/src/main.cpp
@@ -1,13 +1,20 @@
+//============================================================================
+// Name        : main.cpp
+// Description : C++ program for implementation of the AlgorithmComplexity program
+//============================================================================
 #include<iostream>
 #include<utility>
 #include"Logic.hpp"
 
-int main(){
-	int key=1;
-	while(key){
+// The main function of the program
+int main() {
+	int key = 1;
+	while (key) {
+
+		// Call logic function responsible for whole logic of the program
 		logic();
-		std::cout<<"Press zero to exit program. Press any other number to continue. "<<std::endl;
-		std::cin>>key;
+		std::cout << "Press zero to exit program. Press any other number to continue. " << std::endl;
+		std::cin >> key;
 	}
 	return 0;
 }
