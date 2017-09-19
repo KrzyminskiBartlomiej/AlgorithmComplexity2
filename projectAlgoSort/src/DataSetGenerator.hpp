4ca98cb57beaab860af56ea1b274f48a7858f14c
Piotr Wroblewski <piotr.p.wroblewski@ericsson.com>
2017-09-19 09:11:51 +0200

    Added working version of DataSetGenerator

--- projectAlgoSort/src/DataSetGenerator.hpp
+++ projectAlgoSort/src/DataSetGenerator.hpp
@@ -1,27 +1,22 @@
 #ifndef DATASETGENERATOR_HPP_
 #define DATASETGENERATOR_HPP_
-#include<vector>
-#include<algorithm>
-#include <random>
-#include <chrono>
+#include <vector>
+#include <ctime>
 #include <cstdlib>
 
-class DataSetGenerator
-{
+class DataSetGenerator {
 public:
-	DataSetGenerator();
-	void createRandomSet(std::vector<int>& toFill, unsigned long long vectorSize);
-	void createSortedSet(std::vector<int>&toFill, unsigned long long vectorSize);
-	void createBackwardSortedSet(std::vector<int>& toFill, unsigned long long vectorSize);
-	void createSortedSetWithFirstRandomValue(std::vector<int>& toFill, unsigned long long vectorSize);
-	void createBackwardSortedSetWithLastRandomValue(std::vector<int>& toFill, unsigned long long vectorSize);
-	void extendRandomSet(std::vector<int>& toFill, unsigned long long toAdd);
-	void extendSortedSet(std::vector<int>&toFill, unsigned long long toAdd);
-	void extendBackwardSortedSet(std::vector<int>& toFill, unsigned long long toAdd);
-	void extendSortedSetWithFirstRandomValue(std::vector<int>& toFill, unsigned long long toAdd);
-	void extendBackwardSortedSetWithLastRandomValue(std::vector<int>& toFill, unsigned long long toAdd);
+	void createSortedSet(std::vector<int>& toFill, unsigned int sizeOfVector);
+	void createRandomSet(std::vector<int>& toFill, unsigned int sizeOfVector);
+	void createBackwardSortedSet(std::vector<int>& toFill, unsigned int sizeOfVector);
+	void createSortedSetWithFirstRandomValue(std::vector<int>& toFill, unsigned int sizeOfVector);
+	void createBackwardSortedSetWithLastRandomValue(std::vector<int>& toFill, unsigned int sizeOfVector);
+
+	void extendSortedSet(std::vector<int>& toExtend, unsigned long long toAdd);
+	void extendRandomSet(std::vector<int>& toExtend, unsigned long long toAdd);
+	void extendBackwardSortedSet(std::vector<int>& toExtend, unsigned long long toAdd);
+	void extendSortedSetWithFirstRandomValue(std::vector<int>& toExtend, unsigned long long toAdd);
+	void extendBackwardSortedSetWithLastRandomValue(std::vector<int>& toExtend, unsigned long long toAdd);
 };
-
-
 
 #endif /* DATASETGENERATOR_HPP_ */
